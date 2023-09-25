e0f292e0a4d5859500a41d99e4c81abf75ba7e3b
BWNC-code <bryn.wheeler@gmail.com>
2023-09-25 13:34:06 +0000

    add comment and likes count to posts with filters

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,20 +9,16 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
     like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
-        if value.size > 1024*1024*2:
-            raise serializers.ValidationError(
-                'Image size greater than 2MB!'
-            )
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError("Image size greater than 2MB!")
         if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width greater than 4096px'
-            )
+            raise serializers.ValidationError("Image width greater than 4096px")
         if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height greater than 4096px'
-            )
+            raise serializers.ValidationError("Image height greater than 4096px")
         return value
 
     def get_is_owner(self, obj):
@@ -30,12 +26,11 @@ class PostSerializer(serializers.ModelSerializer):
         return request.user == obj.owner
 
     def get_like_id(self, obj):
-        user = self.context['request'].user
+        user = self.context["request"].user
         if user.is_authenticated:
             like = Like.objects.filter(owner=user, post=obj).first()
             return like.id if like else None
         return None
-
 
     class Meta:
         model = Post
@@ -50,6 +45,8 @@ class PostSerializer(serializers.ModelSerializer):
             "is_owner",
             "profile_id",
             "profile_image",
-            'image_filter',
-            'like_id',
+            "image_filter",
+            "like_id",
+            "comments_count",
+            "likes_count",
         ]
